95df76795d21ffe1041497b82a025d07f14bd8e9
jacodegithub <dudeobama246@gmail.com>
2023-11-08 15:17:03 +0530

    commiting section-06-ex-3

---
+++ bootcamp/HTML-CSS/Websites/eco-friendly-website/engine.js
@@ -0,0 +1,11 @@
+// import img from './images/recycle-products.jpg'
+
+// const image = document.querySelector('.header-img')
+
+// console.log('image->', image)
+// window.addEventListener('resize', () => {
+//     const screenWidth = window.innerWidth;
+//     if(screenWidth < 800) {
+//         image.src = img;
+//     }
+// })

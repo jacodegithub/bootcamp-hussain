48dce5ee2e2c7b9f19270347cc800078fede5879
jacodegithub <dudeobama246@gmail.com>
2023-11-13 21:23:05 +0530

    commiting section-03, ex-08

---
+++ bootcamp/JAVASCRIPT/section-03/ex-08/index.js
@@ -0,0 +1,16 @@
+// Write a function called composeWithReduce that takes an array of functions and returns a new function. The new function should be the composition of all the functions in the array, achieved using Array.reduce().
+
+const composedWithReduce = function(functions) {
+    return function(x) {
+        return functions.reduce((result, func) => func(result), x)
+    }
+}
+
+const addThree = x => x + x + x;
+const multiplyByFive = x => x * 5;
+const square = x => x * x;
+
+const composeFn = composedWithReduce([addThree, multiplyByFive, square]);
+const answer = composeFn(2);
+
+console.log(answer);

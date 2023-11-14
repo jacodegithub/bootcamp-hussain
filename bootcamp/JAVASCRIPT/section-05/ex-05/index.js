35729b660e602e69efd24c9c6034835b7eadd978
jacodegithub <dudeobama246@gmail.com>
2023-11-14 19:03:17 +0530

    section-05, ex-05

--- bootcamp/JAVASCRIPT/section-05/ex-05/index.js
+++ bootcamp/JAVASCRIPT/section-05/ex-05/index.js
@@ -1,12 +1,26 @@
-// Modify the fetchUserData function to call processUserData after successfully fetching the data. Ensure that the processing step occurs only if the API call is successful.
+// Provide an example of how to use these functions. Call fetchMultipleUsers with an array of user IDs to demonstrate the asynchronous fetching and processing of user data.
 
-async function fetchUserData(userId) {
-    await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`).then((response) => {
-        let data = response.data;
-        console.log(data);
-    }).catch((err0r) => {
-        console.log(error);
-    })
+async function fetchMultipleUsers(userIds) {
+
+    try{
+        const data = userIds.map(userId => 
+            fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)    
+        )
+
+        const userPromises = await Promise.all(data);
+
+        const userArray = await Promise.all(userPromises.map(
+            res => {
+                if(!res.ok) {
+                    throw new Error(`Found no user with usre`)
+                }
+
+                return res.json();
+            }
+        ))
+         console.log(userArray);
+    }
+    catch(err) {
+        console.log(err);
+    }
 }
-
-fetchUserData(4);

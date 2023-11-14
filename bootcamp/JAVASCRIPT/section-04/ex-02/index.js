b886f27efb97c7841e40c07b9ac77d4d7a018ffb
jacodegithub <dudeobama246@gmail.com>
2023-11-14 12:12:04 +0530

    commiting section-04, ex-02

---
+++ bootcamp/JAVASCRIPT/section-04/ex-02/index.js
@@ -0,0 +1,11 @@
+// Create a class called Library with a property books that is an array to store books. Implement a constructor that initializes an empty array.
+
+class Library {
+    constructor() {
+        this.books = [];
+    }
+}
+
+const lib = new Library();
+
+console.log(lib);

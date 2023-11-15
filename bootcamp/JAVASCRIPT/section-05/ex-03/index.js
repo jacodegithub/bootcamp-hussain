29a72b433773b3974d047b7de7d1f8130339ddac
jacodegithub <dudeobama246@gmail.com>
2023-11-15 11:00:46 +0530

    section-05-ex-03

--- bootcamp/JAVASCRIPT/section-05/ex-03/index.js
+++ bootcamp/JAVASCRIPT/section-05/ex-03/index.js
@@ -1,17 +1,21 @@
 // Modify the fetchUserData function to call processUserData after successfully fetching the data. Ensure that the processing step occurs only if the API call is successful.
 
 async function fetchUserData(userId) {
+    let somedata;
     try {
         const userData = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
 
         if(userData.ok == false) throw new Error("found no user!")
 
         const data = await userData.json();
-        console.log(data);  
+        somedata = data;
+        // console.log(data);  
     }
     catch(err) {
         console.log(err);
     }
+
+    return somedata;
 }
 
-fetchUserData();
+fetchUserData(2).then(response => console.log('response ',response));
